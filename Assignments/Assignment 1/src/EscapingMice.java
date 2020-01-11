91f8e1db80fe9ac6e9d51393da2b59dca9cea1f1
Michael Buffone <michaelbuffonee@gmail.com>
2020-01-11 17:02:37 -0500

    +

--- Assignments/Assignment 1/src/EscapingMice.java
+++ Assignments/Assignment 1/src/EscapingMice.java
@@ -1,3 +1,4 @@
+
 /*
  * Assignment 1
  * Michael Buffone
@@ -9,46 +10,84 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class EscapingMice {	
-	
+public class EscapingMice {
+
 	// Data members for the board
 	private static KeyPoint mouse;
 	private static KeyPoint exit;
-	private static char[][] board;
-	
+	private static KeyPoint[][] board;
+	private static KeyPoint[][] solutionBoard;
+
 	public static void main(String[] args) {
 
-		// Create the file object and read the data into the arrays
-		File file = new File("testMaze.txt");
-		readFile(file);
-		printBoard(board);
+		// ****** Maze 1 ******
+		instructions("testMaze.txt");
+
+		// ****** Maze 2 ******
+		instructions("testMaze2.txt");
+
+		// ****** Maze 3 ******
+		instructions("testMaze3.txt");
 		
-		
+		// ****** Maze 4 ******
+		instructions("testMaze4.txt");
+
+		// Modify these four lines with the getX and getY functions to test the board
+		// "coordination"
+		// - it may seem confusing, but it makes sense once you play around with it
+		// System.out.println("\n" + mouse.getX());
+		// System.out.println(mouse.getY() + "\n---------");
+		// System.out.println(board[mouse.getY()-2][mouse.getX()].getVal());
+		// System.out.println(board[mouse.getY()-2][mouse.getX()].getVal() == '1');
 
 	}
 
 	// ****** I/O Methods ******
+	public static void instructions(String fileName) {
+
+		// Create the file object and read the data into the arrays
+		readFile(new File(fileName));
+
+		printInfo(board);
+		printBoard(board);
+
+		if (solveBoard(board, mouse, exit)) {
+			System.out.println("\nThe mouse found the exit.\n");
+			printBoard(solutionBoard);
+		} else {
+			System.out.println("\nAn exit cannot be found.");
+		}
+
+		System.out.println("\n-----------------------------\n");
+
+	}
+
 	public static void readFile(File file) {
 		// Attempt scanner connection to the file
 		try {
 			Scanner input = new Scanner(file);
-			
+
 			try {
-				
+
 				// Get the board dimensions and create it based on those
 				int numRow = input.nextInt();
 				int numCol = input.nextInt();
-				board = new char[numRow][numCol];
-				
+				board = new KeyPoint[numRow][numCol];
+				solutionBoard = new KeyPoint[numRow][numCol];
+
 				// For every element in the text file, add it to the board
 				// -If the element is a mouse or exit, create the keypoint for it
-				for(int i = 0; i < numRow; i++) {
-					for(int j = 0; j < numCol; j++) {
-						
+				for (int i = 0; i < numRow; i++) {
+					for (int j = 0; j < numCol; j++) {
+
 						String curr = input.next();
-						if(curr.equals("m")) mouse = new KeyPoint(i, j);
-						else if(curr.equals("e")) exit = new KeyPoint(i, j);
-						board[i][j] = curr.charAt(0);				
+						if (curr.equals("m"))
+							mouse = new KeyPoint(j, i, 'm');
+						else if (curr.equals("e"))
+							exit = new KeyPoint(j, i, 'e');
+						board[i][j] = new KeyPoint(j, i, curr.charAt(0));
+						solutionBoard[i][j] = new KeyPoint(j, i, curr.charAt(0));
+
 					}
 				}
 
@@ -61,23 +100,89 @@ public class EscapingMice {
 			System.out.println("Error reading the data file!");
 		}
 	}
-	
+
 	// ****** Puzzle Related Methods ******
-	public static void solveBoard(char[][] board) {
-		
+	public static boolean solveBoard(KeyPoint[][] board, KeyPoint mouse, KeyPoint exit) {
+
+		// Stack creation and duplicate the board to show the path
+		Stack<KeyPoint> cellStack = new Stack<KeyPoint>();
+
+		// Starting cell is initialized to the mouse's location
+		KeyPoint curCell = new KeyPoint(mouse.getX(), mouse.getY());
+
+		while (!curCell.equals(exit)) {
+			// Mark the current cell as visited
+			curCell.setVisited(true);
+
+			// Push unvisited open neighbours of currentCell onto the stack
+			// -12'oclock, 3'oclock, 6'oclock, 9'oclock cell check order
+			try {
+				if ((board[curCell.getY() - 1][curCell.getX()].getVal() == '0'
+						|| board[curCell.getY() - 1][curCell.getX()].getVal() == 'e')
+						&& (board[curCell.getY() - 1][curCell.getX()].getVisited() == false)) {
+					solutionBoard[curCell.getY() - 1][curCell.getX()].setVal('.');
+					cellStack.push(board[curCell.getY() - 1][curCell.getX()]);
+				}
+
+				if ((board[curCell.getY()][curCell.getX() + 1].getVal() == '0'
+						|| board[curCell.getY()][curCell.getX() + 1].getVal() == 'e')
+						&& (board[curCell.getY()][curCell.getX() + 1].getVisited() == false)) {
+					solutionBoard[curCell.getY()][curCell.getX() + 1].setVal('.');
+					cellStack.push(board[curCell.getY()][curCell.getX() + 1]);
+				}
+
+				if ((board[curCell.getY() + 1][curCell.getX()].getVal() == '0'
+						|| board[curCell.getY() + 1][curCell.getX()].getVal() == 'e')
+						&& (board[curCell.getY() + 1][curCell.getX()].getVisited() == false)) {
+					solutionBoard[curCell.getY() + 1][curCell.getX()].setVal('.');
+					cellStack.push(board[curCell.getY() + 1][curCell.getX()]);
+				}
+
+				if ((board[curCell.getY()][curCell.getX() - 1].getVal() == '0'
+						|| board[curCell.getY()][curCell.getX() - 1].getVal() == 'e')
+						&& (board[curCell.getY()][curCell.getX() - 1].getVisited() == false)) {
+					solutionBoard[curCell.getY()][curCell.getX() - 1].setVal('.');
+					cellStack.push(board[curCell.getY()][curCell.getX() - 1]);
+				}
+			} catch(Exception e) {
+				System.out.println("Issue with the board boundaries detected @ " + curCell);
+			}
+			
+
+			//cellStack.print("Cells");
+
+			// Check if the stack is empty - if it is a solution cannot be found
+			if (cellStack.isEmpty())
+				return false;
+			else {
+				try {
+					curCell = cellStack.pop();
+					//solutionBoard[curCell.getY()][curCell.getX()]
+				} catch (Exception e) {
+					System.out.println("Error popping the cell stack!");
+				}
+			}
+		}
+		if (curCell.getVal() == 'e') {
+			solutionBoard[curCell.getY()][curCell.getX()].setVal('e');
+			return true;
+		} else
+			return false;
 	}
-	
-	public static void printBoard(char[][] board) {
-		
-		System.out.println("Mouse location: " + mouse.toString());
-		System.out.println("Exit location: " + exit.toString());
-		System.out.println("Row amount: " + board.length + " Column amount: " + board[0].length);
-		for(int i = 0; i < board.length; i++) {
-			for(int j = 0; j < board[0].length; j++) {
-				System.out.print(board[i][j] + " ");
+
+	public static void printBoard(KeyPoint[][] board) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				System.out.print(board[i][j].getVal() + " ");
 			}
 			System.out.println();
 		}
+	}
+
+	public static void printInfo(KeyPoint[][] board) {
+		System.out.println("Mouse location: " + mouse.toString());
+		System.out.println("Exit location:  " + exit.toString());
+		System.out.println("Row amount: " + board.length + ", Column amount: " + board[0].length);
 	}
 
 }
--- Assignments/Assignment 1/src/KeyPoint.java
+++ Assignments/Assignment 1/src/KeyPoint.java
@@ -9,10 +9,18 @@
 public class KeyPoint {
 	private int x = 0;
 	private int y = 0;
+	private boolean visited = false;
+	private char val = '-';
 	
 	public KeyPoint(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	public KeyPoint(int x, int y, char val) {
+		this.x = x;
+		this.y = y;
+		this.val = val;
 	}
 
 	// Getters and setters
@@ -28,9 +36,30 @@ public class KeyPoint {
 	public void setY(int y) {
 		this.y = y;
 	}
+	public boolean getVisited() {
+		return visited;
+	}
+	public void setVisited(boolean visited) {
+		this.visited = visited;
+	}
+	public char getVal() {
+		return val;
+	}
+	public void setVal(char val) {
+		this.val = val;
+	}
 	
 	@Override
 	public String toString() {
-		return "x = " + this.x + ", y = " + this.y;
+		return "x = " + this.x + ", y = " + this.y + ", value = " + this.getVal();
+	}
+	
+	public boolean equals(KeyPoint o) {
+		if(this.getX() == o.getX()) {
+			if(this.getY() == o.getY()) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
