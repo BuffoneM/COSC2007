ef491d08532093dd83236523bade80cb2079e257
Michael Buffone <BuffoneM@users.noreply.github.com>
2020-03-06 16:02:01 -0500

    +

--- Assignments/Assignment 3/src/SortedArrayTable.java
+++ Assignments/Assignment 3/src/SortedArrayTable.java
@@ -11,7 +11,7 @@ public class SortedArrayTable<T extends Comparable> {
     }
 
     // Methods
-    public void insert(T obj) {
+    public void insert(T obj) throws Duplicate_Item_Found {
         // Full dictionary case
         if (size == MAX_SIZE) throw new Dictionary_Full("Dictionary full");
         if (obj.equals("")) throw new RuntimeException("Empty string entered");
@@ -36,73 +36,78 @@ public class SortedArrayTable<T extends Comparable> {
 
         // Insert the object in it's proper location
         for (int i = 0; i < size; i++) {
-            try {
-                if (obj.equals(items[i])) throw new Duplicate_Item_Found("Duplicate item being inserted: " + obj);
+            if (obj.equals(items[i])) throw new Duplicate_Item_Found("Duplicate item being inserted: " + obj);
 
-                // Look for the case where it is bigger than the already existing string
-                if (obj.compareTo(items[i]) >= 1) {
+            // Look for the case where it is bigger than the already existing string
+            if (obj.compareTo(items[i]) >= 1) {
 
-                    // Shift the other elements over by 1 element and insert
-                    for (int j = items.length - 2; j >= i; j--) {
-                        items[j + 1] = items[j];
-                    }
-
-                    items[i + 1] = obj;
-                    size++;
-                    return;
+                // Shift the other elements over by 1 element and insert
+                for (int j = items.length - 2; j >= i; j--) {
+                    items[j + 1] = items[j];
                 }
-            } catch(Duplicate_Item_Found dif) {
-                System.out.println("Duplicate item being inserted: " + obj);
-            }
 
+                items[i + 1] = obj;
+                size++;
+                return;
+            }
         }
     }
 
-    public boolean delete(T obj) {
+    public String[] delete(T obj) throws Duplicate_Item_Found {
         if (size == 0) throw new Dictionary_Full("Dictionary is empty");
+        String elements[] = null;
 
         // Return the predecessor and successor of obj if obj would have existed
-        try {
-            if (!search(obj)) throw new Element_Not_Found("");
-        } catch (Element_Not_Found enf) {
-
+        if (!search(obj)) {
+            elements = new String[2];
             System.out.println("Element '" + obj + "' not found");
 
             // If the logical size is 1
             if (size == 1) {
-                if (obj.compareTo(items[0]) < 0)
+                if (obj.compareTo(items[0]) < 0) {
                     System.out.println("Predecessor doesn't exist | Successor: '" + items[0] + "'");
-                else
+                    elements[0] = "";
+                    elements[1] = items[0] + "";
+                } else {
                     System.out.println("Predecessor: '" + items[0] + "' | Successor doesn't exist");
+                    elements[0] = items[0] + "";
+                    elements[1] = "";
+                }
             }
 
             // If the obj is < than the first element
-            else if (obj.compareTo(items[0]) < 0)
+            else if (obj.compareTo(items[0]) < 0) {
                 System.out.println("Predecessor doesn't exist | Successor: '" + items[0] + "'");
-
-                // If the obj is > than the last element
-            else if (obj.compareTo(items[size - 1]) > 0)
+                elements[0] = "";
+                elements[1] = items[0] + "";
+            }
+            // If the obj is > than the last element
+            else if (obj.compareTo(items[size - 1]) > 0) {
                 System.out.println("Predecessor: '" + items[size - 1] + "' | Successor doesn't exist");
+                elements[0] = items[size - 1] + "";
+                elements[1] = "";
+            }
 
-                // The obj exists between elements in the array
+            // The obj exists between elements in the array
             else {
                 for (int i = 0; i < size; i++) {
                     if (obj.compareTo(items[i]) >= 1) {
                         if (obj.compareTo(items[i + 1]) < 0) {
                             System.out.println("Predecessor: '" + items[i] + "' | Successor: '" + items[i + 1] + "'");
+                            elements[0] = items[i] + "";
+                            elements[1] = items[i + 1] + "";
                         }
                     }
                 }
             }
-
-            return false;
+            return elements;
         }
 
         // If the last item is being deleted, delete it and return (no need to shift everything)
         if (obj.equals(items[size - 1])) {
             items[size - 1] = null;
             size -= 1;
-            return true;
+            return elements;
         }
 
         // Find the element, delete it, and shift every element ahead of it to the left
@@ -115,30 +120,48 @@ public class SortedArrayTable<T extends Comparable> {
                 for (int j = i; j < size + 1; j++) {
                     items[j] = items[j + 1];
                 }
-
-                return true;
+                return elements;
             }
         }
 
-        return false;
+        return elements;
     }
 
     public boolean search(T obj) {
         if (size == 0) throw new Dictionary_Full("Dictionary is empty");
         for (int i = 0; i < size; i++) {
-            if (obj.equals(items[i])) return true;
+            if (obj.equals(items[i])) {
+                System.out.println("'" + obj + "' (exists at logical position: " + (i + 1) + ")");
+                return true;
+            }
         }
+        System.out.println("'" + obj + "' doesn't exist in the dictionary");
         return false;
     }
 
     public void print() {
-        System.out.print("-------------------------------\n" + toString() + "\nPhysical size print:\n[");
+        System.out.print("-------------------------------\n" + toString() + "\n\nPhysical size print:\n[");
         for (int i = 0; i < MAX_SIZE; i++) {
             System.out.print(items[i] + (i == MAX_SIZE - 1 ? "]\n" : ", "));
+        }
+
+        System.out.print("\nLogical size print:\n[");
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i] + (i == size - 1 ? "]\n" : ", "));
         }
         System.out.println("-------------------------------");
     }
 
+    public String[] toArray() {
+        String[] array = null;
+        if(size != 0) {
+            array = new String[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = items[i] + "";
+            }
+        }
+        return array;
+    }
     public String toString() {
         return "Physical size: " + MAX_SIZE + " | Logical size: " + size;
     }
--- Assignments/Assignment 3/src/TestProgram.java
+++ Assignments/Assignment 3/src/TestProgram.java
@@ -34,15 +34,19 @@ public class TestProgram {
         // Attempt to read the file
         File file = new File(fileName);
 
+        // Output file parts
+        File outputFile = new File("output.txt");
+        PrintWriter pw = null;
+
         if (!file.exists()) {
             System.out.println("The file does not exist...\n");
             System.out.println("Complete...\n-----------------------------");
             return;
         }
 
-
         try {
             Scanner input = new Scanner(file);
+            pw = new PrintWriter(outputFile);
             SortedArrayTable<String> dictionary = new SortedArrayTable<String>(100);
 
             while(input.hasNext()) {
@@ -51,33 +55,69 @@ public class TestProgram {
                 if(curr.toUpperCase().charAt(0) != 'P') {
                     currWord = curr.substring(curr.indexOf("(") + 2, curr.indexOf(")") - 1);
                 }
-
+                System.out.println("------");
                 // Method determination
                 switch(curr.toUpperCase().charAt(0)) {
                     case 'D':
                         System.out.println("Deleting: " + currWord);
-                        dictionary.delete(currWord);
+                        String[] elements = null;
+                        try {
+                            elements = dictionary.delete(currWord);
+                            if(elements == null)
+                                pw.println("Element \"" + currWord + "\" Deleted");
+                            else {
+                                pw.println("Element_Not_Found");
+                                if(!elements[0].equals(""))
+                                    pw.println(elements[0]);
+                                if(!elements[1].equals(""))
+                                    pw.println(elements[1]);
+                            }
+                        } catch(Duplicate_Item_Found dif) {
+                            pw.println("Inserted Duplicate Element");
+                        }
+
+
                         break;
+
                     case 'I':
                         System.out.println("Inserting: " + currWord);
-                        dictionary.insert(currWord);
+
+                        try {
+                            dictionary.insert(currWord);
+                            pw.println("Inserted Element \"" + currWord + "\"");
+                        } catch(Duplicate_Item_Found dif) {
+                            pw.println("Inserted Duplicate Element");
+                        }
                         break;
+
                     case 'P':
                         System.out.println("Printing:");
                         dictionary.print();
                         break;
+
                     case 'S':
                         System.out.println("Searching for: " + currWord);
-                        dictionary.search(currWord);
+                        if(dictionary.search(currWord) == false) {
+                            pw.println("Element_Not_Found");
+                        }
                         break;
                     default:
                         System.out.println("Invalid instruction attempted: " + curr);
                         break;
                 }
             }
-
+            // Print all of the elements in the file
+            String[] tempArray = dictionary.toArray();
+            if(tempArray != null) {
+                for(int i = 0; i < tempArray.length; i++) {
+                    pw.println(tempArray[i]);
+                }
+            }
+            pw.close();
         } catch (IOException ioe) {
             System.out.println(ioe.getMessage());
+        } finally {
+            if(pw != null) pw = null;
         }
 
         System.out.println("Instructions complete...");
